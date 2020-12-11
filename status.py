48c2bb0a736509bed7cabec3848f17b1bf0ecb70
anonymous770 <aslammiya5257@gmail.com>
2020-12-11 22:56:37 +0530

    beta

--- status.py
+++ status.py
@@ -1,25 +1,33 @@
 import os
 import shutil
+import subprocess as sub
 
 def create_dir():
 	try:
-			os.mkdir('WhatsApp\ Status')
-			os.chdir('/sdcard/WhatsApp\ Status')
+			os.chdir('/sdcard')
+			os.mkdir('WhatsAppStatus')
+			os.chdir('/sdcard/WhatsAppStatus')
 			os.mkdir('Videos')
 			os.mkdir('Images')
 	except FileExistsError as exer:
 				pass
-				
-def change_dir():
-	try:
-		os.chdir('/sdcard/WhatsApp/Media/.Statuses')
-	except FileNotFoundError as dir_err:
-		print(dir_err)
-		print('WhatsApp folder is not exist.')
-		
-def coppy():
-	os.system('cp -r *.mp4 /sdcard/WhatsApp\ Status')
-	os.system('cp -r *.jpg /sdcard/WhatsApp\ Status')
-	os.system('cp -r *.jpeg /sdcard/WhatsApp\ Status')
-	try:
-		shutil.copy2
+
+src = ('/sdcard/WhatsApp/Media/.Statuses/')
+dst_vid = ('/sdcard/WhatsAppStatus/Videos')
+dst_img = ('/sdcard/WhatsAppStatus/Images')
+
+def pull_status():
+	os.chdir(src)
+	#os.system(f'cp -r *.mp4 {dst_vid}')
+	os.system('''
+
+
+''')
+	os.system(f'cp -r *.jpg {dst_img}')
+
+def main():
+	create_dir()
+	pull_status()
+
+#create_dir()
+main()
